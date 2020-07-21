13a6e896da952afefe47a2d08dfa45d8693a3604
Ethan <190196539@qq.com>
2020-07-21 14:53:18 +0800

    dev

--- src/It266Sms.php
+++ src/It266Sms.php
@@ -4,7 +4,7 @@ namespace SMS;
 
 use PFinal\Http\Client;
 
-class It266Sms implements SmsVerifyInterface
+class It266Sms implements SmsVerifyInterface, SmsTemplateInterface
 {
     public $gateway;
     public $appId;
@@ -32,5 +32,26 @@ class It266Sms implements SmsVerifyInterface
     private function getSign()
     {
         return strtolower(md5($this->appId . $this->appSecret));
+    }
+
+    public function templateSMS($phone, $templateId = null, $params = array(), &$error = null)
+    {
+        $client = new Client();
+        $res = $client->post($this->gateway . '/api/sms/send/template', array('mobile' => $phone, 'template' => $templateId, 'content' => json_encode($params),
+            'app_id' => $this->appId, 'sign' => $this->getSign()));
+
+        if (!$res->getStatusCode() == 200) {
+            $error = 'http error ' . $res->getStatusCode();
+            return false;
+        }
+
+        $body = $res->getBody();
+        $json = @json_decode($body, true);
+        if (is_array($json) && array_key_exists('code', $json) && $json['code'] === 'SUCCESS') {
+            return true;
+        }
+
+        $error = $body;
+        return false;
     }
 }
